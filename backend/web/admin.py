2bf58058d74884222ac29ef1c0bd86c530d5c03a
Rijan Bhattarai <rijanbhattarai2006@gmail.com>
2026-06-20 23:35:58 +0545

    Remove unnecessary folders

--- backend/web/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TherapistProfile, ClientProfile, Appointment, Message, Review
-
-admin.site.register(TherapistProfile)
-admin.site.register(ClientProfile)
-admin.site.register(Appointment)
-admin.site.register(Message)
-admin.site.register(Review)
